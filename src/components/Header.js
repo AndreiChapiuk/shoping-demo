b22b6e6a88664cd8d729b48bd8996e3746e1ed2c
Andrei Chapiuk <v52405442@gmail.com>
2025-07-17 10:37:49 +0200

    feat: "add navigation in header block"

---
+++ src/components/Header.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <header>
+        <div>
+            <span className='logo'>Peptides</span>
+            <ul className='navigation'>
+              <li>КОМАНДА</li>
+              <li>СВОЙСТВА</li>
+              <li>ОПТОВЫЕ ЦЕНЫ</li>
+              <li>О ПЕПТИДАХ</li>
+
+            </ul>
+        </div>
+        <div className='presentation'>
+
+        </div>
+    </header>
+  )
+}
